f666abde1ed4910a94fd64e073273379f049499d
SandibelS <17-10614@usb.ve>
2024-04-03 23:48:50 -0400

    Agregado chequeo de codigo de materia y comentarios

--- proyecto2/main.c
+++ proyecto2/main.c
@@ -1,14 +1,29 @@
-#include <stdio.h>  /* printf */
-#include <stdlib.h> /* EXIT_FAILURE, EXIT_SUCCESS*/
-#include <string.h> /* strlen(), strcpy(), strcmp()*/
+/*  Proyecto 2. Pide Cola
+    Participantes:
+        Sandibel Soares
+        Carlo Herrera
+        Juan Cuevas
 
-# include "calc_prob.h"
-# include "files_finder.h"
-# include "queue.h"
+    Archivo: main.c
+    Procesa el input del usuario dado por la línea de comandos.
+    Proporciona mensajes de ayuda si se introduce mal alguna flag.
+    Imprime mensajes de bienvenida y despedida.
+    Dependiendo del input la función main decide qué acción realizar:
+        Calcular la probabilidad asociada a un carnet.
+        Calcular la cantidad de carros asociados a una materia.
+*/
+
+#include <stdio.h>  /* funcion printf */
+#include <stdlib.h> /* constantes EXIT_FAILURE, EXIT_SUCCESS */
+#include <string.h> /* funciones strlen, strcpy, strcmp */
+
+# include "calc_prob.h"     /* funciones calc_prob, car_prob */
+# include "files_finder.h"  /* funciones find_asignatures, find_students*/
+# include "queue.h"         /*  Struct Queue; funciones peek, dequeue*/
 
 /* Funcion que procesa los argumentos pasados por la linea de comando
    Retorna:
-    -1 si hay errorres en los argumentos,
+    -1 si hay errores en los argumentos,
     0  si se tiene que calcular la probabilidad asociada a un carnet,
     1  si se tiene que calcular la cantidad de carros de una materia
 */
@@ -33,7 +48,7 @@ int process_arguments(int argc, char const *argv[], char carnet[10], char course
     
 
     /* Hay que verificar si se pasó un carnet o el codigo de una materia (y que sean validos)*/
-    /* asumiendo que los carnets siempre tiene 7 caracteres */
+    /* asumiendo que los carnets siempre tienen 7 caracteres */
     if (strlen(argv[1]) == 7)
     {
         /* si todos los caracteres estan entre [48, 57]  (los ascii de 0 hasta 9) 
@@ -45,6 +60,7 @@ int process_arguments(int argc, char const *argv[], char carnet[10], char course
             if (argv[1][i] < 48 || argv[1][i] > 57)
             {
                 printf("\033[91;1mError:\033[0m carnet invalido \n");
+                printf("Use %s -h/--help para obtener ayuda\n", argv[0]);
                 return -1;
             }
             
@@ -53,11 +69,42 @@ int process_arguments(int argc, char const *argv[], char carnet[10], char course
        strcpy(carnet, argv[1]);
        result = 0;
     }
-    /* Asumiendo que los codigos de materia siempre tienen 6 caracteres (??)*/
+    /* Asumiendo que los codigos de materia siempre tienen 6 caracteres */
     else if (strlen(argv[1]) == 6){
 
-        /* podemos revisar si los primeros dos caracteres del argumento NO estan entre
-           [48, 57], esto podria indicar que es un cod de materia*/
+        /* Nos estan pasando el codigo de una materia si:
+            Los dos primeros caracteres estan entre [65, 90] (los ascii de A a Z)
+            Si el tercer caracter esta entre [65, 90] (A...Z) o [48, 57] (0..9)
+            El resto de caracteres estan entre [48, 57] (0..9)
+        */
+        int i;
+        for (i = 0; i < 2; i++)
+        {
+            if (argv[1][i] < 65 || argv[1][i] > 90)
+            {
+                printf("\033[91;1mError:\033[0m Codigo de materia invalido \n");
+                printf("Use %s -h/--help para obtener ayuda\n", argv[0]);
+                return -1;
+            }
+        }
+
+        if ( (argv[1][2] < 65 || argv[1][2] > 90) && (argv[1][2] < 48 || argv[1][2] > 57))
+        {
+            printf("\033[91;1mError:\033[0m Codigo de materia invalido \n");
+            printf("Use %s -h/--help para obtener ayuda\n", argv[0]);
+            return -1;
+        }
+
+        for (i = 3; i < 6; i++)
+        {
+            if (argv[1][i] < 48 || argv[1][i] > 57)
+            {
+                printf("\033[91;1mError:\033[0m Codigo de materia invalido \n");
+                printf("Use %s -h/--help para obtener ayuda\n", argv[0]);
+                return -1;
+            }
+        }
+
         strcpy(course_code, argv[1]);
         result = 1; 
     }
@@ -68,6 +115,8 @@ int process_arguments(int argc, char const *argv[], char carnet[10], char course
         return -1;
     }
     
+    
+    /* Se procesan los posibles flags pasados */
     int i;
     for (i = 2; i < argc; i += 2)
     {   
@@ -109,7 +158,6 @@ int process_arguments(int argc, char const *argv[], char carnet[10], char course
 
         if (strcmp(argv[i], "-d")  == 0 )
         {
-            /* deberiamos comprobar aqui que el directorio existe? */
             strcpy(root_dir, argv[i+1]);
 
         }
@@ -121,6 +169,7 @@ int process_arguments(int argc, char const *argv[], char carnet[10], char course
 
 }
 
+/* Funcion que imprime mensajes de ayuda con respecto al uso del programa y las flags */
 void print_help(char const *argv[]){
     printf("\033[93;1mUso:\033[0m %s <carnet|codigo_materia> [opciones]\n", argv[0]);
     printf("Calcula la probabilidad de que un estudiante o una seccion pueda subir a la universidad por cola\n");
@@ -135,6 +184,7 @@ void print_help(char const *argv[]){
     printf("  %s 1910056\n", argv[0]);
 }
 
+/* Funcion que imprime la bienvenida al programa */
 void print_welcome(){
     printf("👋 \033[93;1mBienvenido al sistema de análisis de datos de PideCola 3.0\033[0m 👋\n");
     printf("💻 \033[93;1mDesarrollado por:\033[0m\n");
@@ -143,33 +193,51 @@ void print_welcome(){
     printf("  \033[93;1m-\033[0m Juan Cuevas\n");
 }
 
+/* Funcion que imprime la despedida al finalizar el programa */
 void print_goodbye(){
     printf("\n");
     printf("👋 \033[93;1mGracias por usar el sistema de análisis de datos de PideCola 3.0\033[0m 👋\n");
     printf("\033[93;1mPD:\033[0m Alguien por favor que nos de una cola, nos quedamos en la uni 🥺\n");
 }
 
+/*  Funcion principal. 
+    Llama a process_arguments para procesar el input
+    Si no hubo errores, se decide qué acción realizar:
+        Calcular la probabilidad asociada a un carnet.
+        Calcular la cantidad de carros asociados a una materia
+    Si alguna funcion reporta un error se retorna EXIT_FAILURE, 
+    EXIT_SUCCESS de lo contrario.
+*/
 int main(int argc, char const *argv[])
 {
     /* Variables */
+
+    /* Arreglo de caracteres en donde se guarda el carnet pasado */
     char  carnet[10];
+    /* Arreglo de caracteres en donde se guarda el codigo de una materia pasada */
     char  course_code[10];
+    /* Ultima corte de la universidad, por defecto la 21 */
     int   last_cohort = 21;
+    /* Probabilidad de que la ultima corte tenga un carro, por defecto 3 */
     float p_cohort = 3;
+    /* Incremento de la probabilidad de tener carro con respecto a la ultima corte */
     float increase = 4;
-    char  root_dir[50];
+    /* Ruta a la carpeta principal que contiene todos los archivos de DACE, por defecto  "./DACE"*/
+    char  root_dir[100];
     strcpy(root_dir, "./DACE");
 
+    /* Se comprueba si el usuario esta pidiendo ayuda para el uso del programa */
     if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0))
     {
         print_help(argv);
         return EXIT_SUCCESS;
     }
 
-    /* Procesar los argumentos pasados por la linea de comandos */
+    /* Se procesan los argumentos pasados por la linea de comandos */
     int r = process_arguments(argc, argv, carnet, course_code, \
                               &last_cohort, &p_cohort, &increase, root_dir);
 
+    /* Si r == -1, hay un error en los argumentos pasados y se aborta el program */
     if (r == -1)
     {
         return EXIT_FAILURE;
@@ -177,11 +245,13 @@ int main(int argc, char const *argv[])
 
     print_welcome();
 
-    
+
     if (r == 0)
     {
-        printf("🧮 Vamos a calcular la probabilidad de que el estudiante \033[92;1m%s\033[0m pueda subir a la universidad en cola 🧮\n", carnet);
         /* Si r == 0, llamamos el codigo que calcula la proba por carnet*/
+
+        printf("🧮 Vamos a calcular la probabilidad de que el estudiante \033[92;1m%s\033[0m pueda subir a la universidad en cola 🧮\n", carnet);
+        
         Queue * asignatures = find_asignatures(root_dir, carnet);
         if (asignatures == NULL)
         {
@@ -194,7 +264,8 @@ int main(int argc, char const *argv[])
     }
     else if ( r == 1)
     {
-        /* Si r == 1, llamamos el codigo que calcula la cantidad de carros por materia */
+        /* Si r == 1, llamamos e\l codigo que calcula la cantidad de carros por materia */
+
         printf("🧮 Vamos a calcular la cantidad promedio de carros para la materia \033[92;1m%s\033[0m y si son suficientes para que todos los estudiantes puedan subir 🧮\n", course_code);
         Queue * students = find_students(root_dir, course_code);
         if (students == NULL)
