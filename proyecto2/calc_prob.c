c4353f478bed835e239a2df16f5d445c3b82b56d
SandibelS <17-10614@usb.ve>
2024-04-05 03:52:17 -0400

    Agregado comentarios explicativos, y eliminadas funciones sin los porcentajes

--- proyecto2/calc_prob.c
+++ proyecto2/calc_prob.c
@@ -1,64 +1,30 @@
-# include <stdio.h>
-# include <math.h>
-# include <string.h>
+/*  Proyecto 2. Pide Cola
+    Participantes:
+        Sandibel Soares
+        Carlo Herrera
+        Juan Cuevas
+
+    Archivo: calc_prob.c
+    Contiene las funciones para:
+        - Calcular la probabilidad de asistencia de un estudiante a 
+        una materia
+        - Calcular la cantidad de carros esperados en una materia y se 
+        verifica si son suficientes para dar cola a los estudiantes que 
+        lo necesiten    
+*/
+
+# include <stdio.h>   /* funcion printf */
+# include <stdlib.h>  /* funcion free */
+# include <math.h>    /* funcion ceil */
+# include <string.h>  /* funcion strcpy */
 
 # include "calc_prob.h"
 
-float student_prob(Queue * students, char* carnet){
-
-    /*
-    Inicializamos la probabilidad acumulada en 25 contando el 15% de llegar 
-    en el bus de la universidad y el 10% de llegar por transporte público.
-    */
-    float prob = 25.0;
-    int cohorte;
-
-    char *line;
-
-    /*Leemos cada linea de estudiantes*/
-    while(students->length != 0){
-
-        /*Leemos la linea*/
-        strcpy(line, peek(students));
-
-        /*Extraemos la cohorte como entero*/
-        cohorte = (line[0] - '0') * 10 + (line[1] - '0');
-        
-        /*
-        Si es una cohorte posterior al 2000 sumamos 100 para mantener
-        la distancia entre dos cohortes.
-        */
-        if(cohorte < 70 ){
-            cohorte += 100;
-        }
-
-        /*Sumamos la probabilidad del estudiante de tener carro segun su cohorte al acumulado.*/
-        prob += 3 + (121-cohorte)*4;
-
-        dequeue(students);
-    }
-
-    /*Extraemos la cohorte del estudiante por el que se consulta como entero.*/
-    cohorte = (carnet[0] - '0') * 10 + (carnet[1] - '0');
-
-    /*
-    Si el estudiante por el que se consulta es de una cohorte posterior al 2000, sumamos 100 
-    para mantener la distancia entre dos cohortes.
-    */
-    if(cohorte < 70 ) cohorte += 100;
-
-    /*
-    Restamos la probabilidad del estudiante por el que se consulta de tener carro segun su 
-    cohorte al acumulado.
-    */
-    prob -= 3 + (121-cohorte)*4;
-
-    /*La probabilidad maxima es de 100%*/
-    if(prob > 100) prob = 100;
-
-    return prob;
-}
-
+/*  
+    Calcula la probabilidad de asistencia de un estudiante a una materia 
+    dado su carnet y una cola con los carnets de los estudiantes inscritos a 
+    la materia
+*/
 float student_prob_default(Queue * students, char* carnet, float p, float q){
 
     /*
@@ -68,7 +34,7 @@ float student_prob_default(Queue * students, char* carnet, float p, float q){
     float prob = 25;
     int cohorte;
 
-    char *line;
+    char line[10];
 
     /*Leemos cada linea de estudiantes*/
     while(students->length != 0){
@@ -88,6 +54,8 @@ float student_prob_default(Queue * students, char* carnet, float p, float q){
         /*Sumamos la probabilidad del estudiante de tener carro segun su cohorte al acumulado.*/
         prob += p + (121-cohorte)*q;
 
+        /* Liberamos el string y el nodo*/
+        free(peek(students));
         dequeue(students);
     }
 
@@ -104,7 +72,7 @@ float student_prob_default(Queue * students, char* carnet, float p, float q){
     Restamos la probabilidad del estudiante por el que se consulta de tener carro segun su 
     cohorte al acumulado.
     */
-    prob -= 3 + (121-cohorte)*4;
+    prob -= p + (121-cohorte)*q;
 
     /*La probabilidad maxima es de 100%*/
     if(prob > 100) prob = 100;
@@ -112,55 +80,18 @@ float student_prob_default(Queue * students, char* carnet, float p, float q){
     return prob;
 }
 
-void car_prob(Queue * students, char* asignature){
+/*
+    Calcula la cantidad de carros esperados en una materia y se verificar 
+    si son suficientes para dar cola a los estudiantes que lo necesiten
+    Recibe una cola con los carnets de los estudiantes inscritos en la materia,
+    el codigo de la matera, la probabilidad p de que los estudiantes de la ultima
+    corte tengan un carro y el incremento q de la proporción de carro-habientes de 
+    una cohorte a la anterior
 
-    char* line;
-
-    int cohorte;
-    float cars = 0;
-    float num_estudiantes = 0;
-    int i;
-    
-    /*Leemos cada linea de estudiantes*/
-    while( students->length != 0){
-
-        strcpy(line, peek(students));
-
-        num_estudiantes++;
-
-        /*Extraemos la cohorte como entero*/
-        cohorte = (line[0] - '0') * 10 + (line[1] - '0');
-        
-        /*
-        Si es una cohorte posterior al 2000 sumamos 100 para mantener
-        la distancia entre dos cohortes.
-        */
-        if(cohorte < 70 ) cohorte += 100;
-
-        /*Sumamos la probabilidad del estudiante de tener carro segun su cohorte al acumulado.*/
-        cars += 0.03 + (121-cohorte)*0.04;
-
-        dequeue(students);
-    }
-
-    num_estudiantes -= ceil(num_estudiantes * 0.25);
-
-    printf("Para la asignatura ");
-    for(i = 0; i < 6; i++){
-        printf("%c", asignature[i]);
-    }
-    printf(" se esperan %.2f carros. \n", cars);
-
-    if((cars * 3) - num_estudiantes >= 0){
-        printf("La cantidad de carros esperados es suficiente para la cantidad de estudiantes. \n");
-    } else {
-        printf("La cantidad de carros esperados es insuficiente para la cantidad de estudiantes. \n");
-    }
-}
-
+*/
 void car_prob_default(Queue * students, char* asignature, float p, float q){
 
-    char* line;
+    char line[10];
 
     int cohorte;
     float cars = 0;
@@ -184,8 +115,9 @@ void car_prob_default(Queue * students, char* asignature, float p, float q){
         if(cohorte < 70 ) cohorte += 100;
 
         /*Sumamos la probabilidad del estudiante de tener carro segun su cohorte al acumulado.*/
-        cars += p + (121-cohorte)*q;
+        cars += (p + (121-cohorte)*q)/100;
 
+        free(peek(students));
         dequeue(students);
     }
 
--- proyecto2/calc_prob.h
+++ proyecto2/calc_prob.h
@@ -5,16 +5,6 @@
 
 /*
 Funcion que toma los estudiantes de una sección y calcula la probabilidad de que un 
-estudiante pueda conseguir cola con alguno de sus compañeros con la probabilidad inicial
-y el factor de incremento por defecto.
-@param students: arreglo de strings con los carnets de los estudiantes de un curso.
-@param carnet: carnet del estudiante por el que se consulta.
-@return: probabilidad porcentual de que el estudiante pueda conseguir cola.
-*/
-float student_prob(Queue * students, char* carnet);
-
-/*
-Funcion que toma los estudiantes de una sección y calcula la probabilidad de que un 
 estudiante pueda conseguir cola con alguno de sus compañeros.
 @param students: arreglo de strings con los carnets de los estudiantes de un curso.
 @param carnet: carnet del estudiante por el que se consulta.
@@ -27,21 +17,10 @@ float student_prob_default(Queue * students, char* carnet, float p, float q);
 /*
 Funcion que toma los estudiantes de una sección y calcula la cantidad de carros esperados 
 para la materia y si esta es suficiente para que todos puedan subir. 
-Se calcula con la probabilidad inicial y el factor de incremento por defecto.
-@param students: arreglo de strings con los carnets de los estudiantes de un curso.
-@param asignature: código de la asignatura por la que se consulta.
-@return: cantidad de carros esperados para la materia y si es o no suficiente para la cantidad de estudiantes.
-*/
-void car_prob(Queue * students, char* asignature);
-
-/*
-Funcion que toma los estudiantes de una sección y calcula la cantidad de carros esperados 
-para la materia y si esta es suficiente para que todos puedan subir. 
 @param students: arreglo de strings con los carnets de los estudiantes de un curso.
 @param asignature: código de la asignatura por la que se consulta.
 @param p: probabilidad porcentual de que un estudiante de la cohorte mas reciente tenga carro.
 @param q: incremento de la proporción de carro-habientes de una cohorte a la anterior. 
-@return: probabilidad porcentual de que el estudiante pueda conseguir cola.
 */
 void car_prob_default(Queue * students, char* asignature, float p, float q);
 
